6b9a7266f8101893990949f43404629243d3151c
Miguel <miguel-sousav@hotmail.com>
2025-05-22 16:42:15 -0400

    Revert "first commit"
    This reverts commit 584e545d38c3752fbabc9db20692078df59ed9d7.

--- index.js
+++
@@ -1,7 +0,0 @@
-const app = require("./app");
-const http = require("http");
-const server = http.createServer(app);
-
-server.listen(6002, () => {
-	console.log("El servidor esta corriendo en el puerto 6002");
-});
